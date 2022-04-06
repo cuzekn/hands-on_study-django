cafd1f959953c16cbebc8c28639f7c0e2f104f83
cuze <cuzekn@gmail.com>
2022-04-06 23:25:42 +0900

    feat: 指定したIDのデータを取得

--- src/components/UdemyApiFetch.js
+++ src/components/UdemyApiFetch.js
@@ -1,25 +1,59 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export const UdemyApiFetch = () => {
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState([]);
+  const [selectedTask, setSelectedTask] = useState([]);
+  const [id, setId] = useState(1);
 
   useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/tasks/', {
-      headers: {
-        'Authorization': 'Token 348c33a73b1ea526e03ff993aa1b330b02922fa7'
-      }
-    })
-    .then(res => {setTasks(res.data)})
-  }, [])
+    axios
+      .get("http://127.0.0.1:8000/api/tasks/", {
+        headers: {
+          Authorization: "Token 348c33a73b1ea526e03ff993aa1b330b02922fa7",
+        },
+      })
+      .then((res) => {
+        setTasks(res.data);
+      });
+  }, []);
+
+  const getTask = () => {
+    axios
+      .get(`http://127.0.0.1:8000/api/tasks/${id}/`, {
+        headers: {
+          Authorization: "Token 348c33a73b1ea526e03ff993aa1b330b02922fa7",
+        },
+      })
+      .then((res) => {
+        setSelectedTask(res.data);
+      });
+  };
 
   return (
     <div>
       <ul>
-        {
-          tasks.map(task => <li key={task.id}> {task.title} {task.id}</li>)
-        }
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.title} {task.id}
+          </li>
+        ))}
       </ul>
+      Set id <br />
+      <input
+        type="text"
+        value={id}
+        onChange={(evt) => {
+          setId(evt.target.value);
+        }}
+      />
+      <br />
+      <button type="button" onClick={() => getTask()}>
+        Get task
+      </button>
+      <h3>
+        {selectedTask.title} {selectedTask.id}
+      </h3>
     </div>
-  )
-}
+  );
+};
