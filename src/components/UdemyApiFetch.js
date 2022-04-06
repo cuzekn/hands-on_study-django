57d37b4c64fee8b6e480d5afc506fc8bcff7496d
cuze <cuzekn@gmail.com>
2022-04-06 23:11:50 +0900

    feat: djangoで作ったタスクの表示

--- src/components/UdemyApiFetch.js
+++ src/components/UdemyApiFetch.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 export const UdemyApiFetch = () => {
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/api/tasks/', {
+      headers: {
+        'Authorization': 'Token 348c33a73b1ea526e03ff993aa1b330b02922fa7'
+      }
+    })
+    .then(res => {setTasks(res.data)})
+  }, [])
+
   return (
-    <div>UdemyApiFetch</div>
+    <div>
+      <ul>
+        {
+          tasks.map(task => <li key={task.id}> {task.title} {task.id}</li>)
+        }
+      </ul>
+    </div>
   )
 }
