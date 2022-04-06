767b6bed00b82cfb352e9b41d2ad46b47f0dc079
cuze <cuzekn@gmail.com>
2022-04-07 00:42:24 +0900

    refactor: 編集せずに削除した時の入力値初期化

--- src/components/UdemyApiFetch.jsx
+++ src/components/UdemyApiFetch.jsx
@@ -41,6 +41,9 @@ export const UdemyApiFetch = () => {
       .then((res) => {
         setTasks(tasks.filter((task) => task.id !== id));
         setSelectedTask([]);
+        if (editedTask.id === id) {
+          setEditedTask({ id: "", title: "" });
+        }
       });
   };
 
